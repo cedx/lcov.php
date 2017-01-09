53444436981f42279ea864d4a9609e8a35d46ef9
Cédric Belin <cedric@belin.io>
2017-01-09 14:30:38 +0100

    Implemented the `Token` class

--- lib/Token.php
+++ lib/Token.php
@@ -15,4 +15,64 @@ final class Token {
    * @var string The coverage data of a branch.
    */
   const BRANCH_DATA = 'BRDA';
+
+  /**
+   * @var string The number of branches found.
+   */
+  const BRANCHES_FOUND = 'BRF';
+
+  /**
+   * @var string The number of branches hit.
+   */
+  const BRANCHES_HIT = 'BRH';
+
+  /**
+   * @var string The end of a section.
+   */
+  const END_OF_RECORD = 'end_of_record';
+
+  /**
+   * @var string The coverage data of a function.
+   */
+  const FUNCTION_DATA = 'FNDA';
+
+  /**
+   * @var string A function name.
+   */
+  const FUNCTION_NAME = 'FN';
+
+  /**
+   * @var string The number of functions found.
+   */
+  const FUNCTIONS_FOUND = 'FNF';
+
+  /**
+   * @var string The number of functions hit.
+   */
+  const FUNCTIONS_HIT = 'FNR';
+
+  /**
+   * @var string The coverage data of a line.
+   */
+  const LINE_DATA = 'DA';
+
+  /**
+   * @var string The number of lines found.
+   */
+  const LINES_FOUND = 'LF';
+
+  /**
+   * @var string The number of lines hit.
+   */
+  const LINES_HIT = 'LH';
+
+  /**
+   * @var string The path to a source file.
+   */
+  const SOURCE_FILE = 'SF';
+
+  /**
+   * @var string A test name.
+   */
+  const TEST_NAME = 'TN';
 }
