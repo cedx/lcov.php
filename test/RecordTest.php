3a8608ac653e5f5b1bfa35685272527f0844b217
Cédric Belin <cedric@belin.io>
2017-01-17 18:01:23 +0100

    Added the skeleton of the `Record` tests

---
+++ test/RecordTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Implementation of the `lcov\test\RecordTest` class.
+ */
+namespace lcov\test;
+use lcov\{Record};
+
+/**
+ * Tests the features of the `lcov\Record` class.
+ */
+class RecordTest extends \PHPUnit_Framework_TestCase {
+}
