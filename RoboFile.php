3acfae45adaf6e371d17afc47b6a61fdaf0f7c8b
Cédric Belin <cedric@belin.io>
2018-08-31 14:14:04 +0200

    Ported the build system to Robo

---
+++ RoboFile.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+use Robo\Tasks;
+
+// Load the dependencies.
+require_once __DIR__.'/vendor/autoload.php';
+
+/**
+ * Provides tasks for the build system.
+ */
+class RoboFile extends Tasks {
+
+  /**
+   * Creates a new task runner.
+   */
+  function __construct() {
+    $path = (string) getenv('PATH');
+    $vendorDir = (string) realpath('vendor/bin');
+    if (strpos($path, $vendorDir) === false) putenv("PATH=$vendorDir".PATH_SEPARATOR.$path);
+  }
+
+  /**
+   * Deletes all generated files and reset any saved state.
+   */
+  function clean(): void {
+    $this->_cleanDir('var');
+    $this->_deleteDir(['build', 'doc/api', 'web']);
+  }
+
+  /**
+   * Uploads the results of the code coverage.
+   */
+  function coverage(): void {
+    $this->_exec('coveralls var/coverage.xml');
+  }
+
+  /**
+   * Builds the documentation.
+   */
+  function doc(): void {
+    $this->_exec('phpdoc');
+    $this->_exec('mkdocs build');
+  }
+
+  /**
+   * Performs the static analysis of source code.
+   */
+  function lint(): void {
+    $this->_exec('php -l example/main.php');
+    $this->_exec('phpstan analyse');
+  }
+
+  /**
+   * Runs the test suites.
+   */
+  function test(): void {
+    $this->taskPhpUnit()->run();
+  }
+
+  /**
+   * Upgrades the project to the latest revision.
+   */
+  function upgrade(): void {
+    $composer = PHP_OS_FAMILY == 'Windows' ? 'C:\Program Files\PHP\share\composer.phar' : '/usr/local/bin/composer';
+    $this->taskExecStack()->stopOnFail()
+      ->exec('git reset --hard')
+      ->exec('git fetch --all --prune')
+      ->exec('git pull --rebase')
+      ->exec("php \"$composer\" update --no-interaction")
+      ->run();
+  }
+
+  /**
+   * Watches for file changes.
+   */
+  function watch(): void {
+    $this->taskWatch()
+      ->monitor(['lib', 'test'], function() { $this->test(); })
+      ->run();
+  }
+}
