6dad8a1ad4ed689bdc24aa39be02c97e96a4d531
Cédric Belin <cedric@belin.io>
2019-08-27 19:47:43 +0200

    Moved the MkDocs settings to the "doc" folder

--- RoboFile.php
+++ RoboFile.php
@@ -53,7 +53,7 @@ class RoboFile extends Tasks {
         ->copy('LICENSE.md', 'doc/about/license.md'))
       ->addTask($this->taskExec('mkdocs build --config-file=etc/mkdocs.yaml'))
       ->addTask($this->taskFilesystemStack()
-        ->remove(['doc/about/changelog.md', 'doc/about/license.md']))
+        ->remove(['doc/about/changelog.md', 'doc/about/license.md', 'web/mkdocs.yaml']))
       ->run();
   }
 
