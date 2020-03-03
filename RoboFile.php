f33c55581ba5001ac32c3b033519699cbfe38404
Cédric Belin <cedric@belin.io>
2020-03-03 11:20:41 +0100

    Enabled the PHPDoc builder

--- RoboFile.php
+++ RoboFile.php
@@ -47,10 +47,12 @@ class RoboFile extends Tasks {
    * @return Result The task result.
    */
   function doc(): Result {
+    $phpdoc = PHP_OS_FAMILY == 'Windows' ? 'php '.escapeshellarg('C:\Program Files\PHP\share\phpDocumentor.phar') : 'phpdoc';
     return $this->collectionBuilder()
       ->addTask($this->taskFilesystemStack()
         ->copy('CHANGELOG.md', 'doc/about/changelog.md')
         ->copy('LICENSE.md', 'doc/about/license.md'))
+      ->addTask($this->taskExec("$phpdoc --config=etc/phpdoc.xml"))
       ->addTask($this->taskExec('mkdocs build --config-file=doc/mkdocs.yaml'))
       ->addTask($this->taskFilesystemStack()
         ->remove(['doc/about/changelog.md', 'doc/about/license.md', 'web/mkdocs.yaml']))
@@ -95,6 +97,11 @@ class RoboFile extends Tasks {
    * @return Result The task result.
    */
   function version(string $component = 'patch'): Result {
-    return $this->taskSemVer()->increment($component)->run();
+    $semverTask = $this->taskSemVer()->increment($component);
+    $version = $semverTask->setFormat('%M.%m.%p')->__toString();
+    return $this->collectionBuilder()
+      ->addTask($semverTask)
+      ->addTask($this->taskReplaceInFile('etc/phpdoc.xml')->regex('/version number="\d+(\.\d+){2}"/')->to("version number=\"$version\""))
+      ->run();
   }
 }
