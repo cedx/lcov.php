687583c44cd59172dc24a2ad231f93e7d9277792
Cédric Belin <cedric@belin.io>
2020-05-09 01:27:30 +0200

    Updated the "doc" build task

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -5,5 +5,6 @@ Set-Location (Split-Path $PSScriptRoot)
 $phpdoc = $IsWindows ? 'php "C:/Program Files/PHP/share/phpDocumentor.phar"' : 'phpdoc';
 Invoke-Expression "$phpdoc --config=etc/phpdoc.xml"
 
-Copy-Item doc/img/favicon.ico doc/api
+if (-not (Test-Path doc/api/images)) { New-Item doc/api/images -ItemType Directory | Out-Null }
+Copy-Item doc/img/favicon.ico doc/api/images
 mkdocs build --config-file=etc/mkdocs.yaml
