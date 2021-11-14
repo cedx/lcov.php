0049be82e91954293d9e11fb970383824f2e0236
Cédric Belin <cedric@belin.io>
2021-11-14 18:27:49 +0100

    Updated the API reference

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -5,6 +5,5 @@ Set-Location (Split-Path $PSScriptRoot)
 $phpdoc = $IsWindows ? 'php "C:/Program Files/PHP/share/phpDocumentor.phar"' : "phpdoc";
 Invoke-Expression "$phpdoc --config=etc/phpdoc.xml"
 
-if (-not (Test-Path docs/api/images)) { New-Item docs/api/images -ItemType Directory | Out-Null }
-Copy-Item doc/img/favicon.ico docs/api/images
-mkdocs build --config-file=etc/mkdocs.yaml
+if (-not (Test-Path docs/images)) { New-Item docs/images -ItemType Directory | Out-Null }
+Copy-Item www/favicon.ico docs/images
