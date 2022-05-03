8314fd29b36b02228b2822530f6d8087ff60563f
Cédric Belin <cedric@belin.io>
2022-05-03 18:46:21 +0200

    Updated the examples

---
+++ example/format_report.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+use Lcov\{FunctionCoverage, LineCoverage, LineData, Report, SourceFile};
+
+/**
+ * Formats coverage data as LCOV report.
+ */
+$sourceFile = new SourceFile(
+	path: "/home/cedx/lcov.php/fixture.php",
+	functions: new FunctionCoverage(found: 1, hit: 1),
+	lines: new LineCoverage(found: 2, hit: 2, data: [
+		new LineData(lineNumber: 6, executionCount: 2, checksum: "PF4Rz2r7RTliO9u6bZ7h6g"),
+		new LineData(lineNumber: 7, executionCount: 2, checksum: "yGMB6FhEEAd8OyASe3Ni1w")
+	])
+);
+
+$report = new Report("Example", [$sourceFile]);
+print $report;
---
+++ example/parse_report.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+use Lcov\Report;
+
+/**
+ * Parses a LCOV report to coverage data.
+ */
+try {
+	$report = Report::fromString(file_get_contents("/path/to/lcov.info"));
+	$count = count($report->sourceFiles);
+	print "The coverage report contains $count source files:";
+	print json_encode($report);
+}
+
+catch (UnexpectedValueException $e) {
+	print $e->getMessage();
+}
