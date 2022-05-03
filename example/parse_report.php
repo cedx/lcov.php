7e03c3a1d00a4fd235361979489ff0aa5c493144
Cédric Belin <cedric@belin.io>
2022-05-03 22:01:28 +0200

    Updated an example [skip ci]

--- example/parse_report.php
+++ example/parse_report.php
@@ -7,7 +7,7 @@ use Lcov\Report;
 try {
 	$report = Report::fromString(file_get_contents("/path/to/lcov.info"));
 	$count = count($report->sourceFiles);
-	print "The coverage report contains $count source files:";
+	print "The coverage report contains $count source files:" . PHP_EOL;
 	print json_encode($report);
 }
 
