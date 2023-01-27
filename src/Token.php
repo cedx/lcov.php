3769ed29a04f97b84c0c786ab9b14cacdfd6961f
Cédric Belin <cedric@belin.io>
2023-01-28 00:48:32 +0100

    Code formatting

--- src/Token.php
+++ src/Token.php
@@ -5,42 +5,68 @@
  */
 enum Token: string {
 
-	/** The coverage data of a branch. */
+	/**
+	 * The coverage data of a branch.
+	 */
 	case branchData = "BRDA";
 
-	/** The number of branches found. */
+	/**
+	 * The number of branches found.
+	 */
 	case branchesFound = "BRF";
 
-	/** The number of branches hit. */
+	/**
+	 * The number of branches hit.
+	 */
 	case branchesHit = "BRH";
 
-	/** The end of a section. */
+	/**
+	 * The end of a section.
+	 */
 	case endOfRecord = "end_of_record";
 
-	/** The coverage data of a function. */
+	/**
+	 * The coverage data of a function.
+	 */
 	case functionData = "FNDA";
 
-	/** A function name. */
+	/**
+	 * A function name.
+	 */
 	case functionName = "FN";
 
-	/** The number of functions found. */
+	/**
+	 * The number of functions found.
+	 */
 	case functionsFound = "FNF";
 
-	/** The number of functions hit. */
+	/**
+	 * The number of functions hit.
+	 */
 	case functionsHit = "FNH";
 
-	/** The coverage data of a line. */
+	/**
+	 * The coverage data of a line.
+	 */
 	case lineData = "DA";
 
-	/** The number of lines found. */
+	/**
+	 * The number of lines found.
+	 */
 	case linesFound = "LF";
 
-	/** The number of lines hit. */
+	/**
+	 * The number of lines hit.
+	 */
 	case linesHit = "LH";
 
-	/** The path to a source file. */
+	/**
+	 * The path to a source file.
+	 */
 	case sourceFile = "SF";
 
-	/** A test name. */
+	/**
+	 * A test name.
+	 */
 	case testName = "TN";
 }
