b1e06ab06bcf60cbaf45b92f2b68805044a51c3b
Cédric Belin <cedric@belin.io>
2023-01-31 16:40:21 +0100

    Updated a code sample

--- example/parse_report.php
+++ example/parse_report.php
@@ -8,7 +8,7 @@ try {
 	$report = Report::parse(file_get_contents("/path/to/lcov.info"));
 	$count = count($report->sourceFiles);
 	print "The coverage report contains $count source files:" . PHP_EOL;
-	print json_encode($report);
+	print json_encode($report, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 }
 catch (InvalidArgumentException $e) {
 	print $e->getMessage();
