0c28faa5a0ef9bf31cbb92a2c7a0feadda04668f
Cédric Belin <cedric@belin.io>
2023-03-10 15:41:54 +0100

    Updated the "Lint" task [skip ci]

--- scripts/Lint.hx
+++ scripts/Lint.hx
@@ -1,13 +1,5 @@
 /** Performs the static analysis of source code. **/
 function main() {
-	Sys.command("lix", ["run", "checkstyle",
-		"--config", "etc/checkstyle.json",
-		"--exitcode",
-		"--source", "scripts"
-	]);
-
-	Sys.command("vendor/bin/phpstan", ["analyse",
-		"--configuration=etc/phpstan.neon",
-		"--memory-limit=256M"
-	]);
+	Sys.command("lix run checkstyle --config etc/checkstyle.json --exitcode --source scripts");
+	Sys.command("vendor/bin/phpstan", ["analyse", "--configuration=etc/phpstan.neon", "--memory-limit=256M"]);
 }
