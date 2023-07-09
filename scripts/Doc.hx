fc4a1bbd279cdc2e065f227a46b4fb3a8c1bdb00
Cédric Belin <cedric@belin.io>
2023-07-09 17:56:06 +0200

    Update the API reference [skip ci]

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -1,13 +1,16 @@
 import haxe.Json;
 import sys.FileSystem;
 import sys.io.File;
+using Lambda;
 
 /** Builds the documentation. **/
 function main() {
 	final version = Json.parse(File.getContent("composer.json")).version;
+	["CHANGELOG.md", "LICENSE.md"].iter(file -> File.copy(file, 'docs/${file.toLowerCase()}'));
 	Tools.replaceInFile("etc/phpdoc.xml", ~/version number="\d+(\.\d+){2}"/, 'version number="$version"');
 
-	if (FileSystem.exists("docs")) Tools.removeDirectory("docs");
+	if (FileSystem.exists("docs/api")) Tools.removeDirectory("docs/api");
 	Sys.command("phpdoc --config=etc/phpdoc.xml");
-	File.copy("www/favicon.ico", "docs/images/favicon.ico");
+	FileSystem.createDirectory("docs/api/images");
+	File.copy("docs/favicon.ico", "docs/api/images/favicon.ico");
 }
