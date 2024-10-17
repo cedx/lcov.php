e741ce99b52b89a64f0f7831efcd8d24be4d8047
Cédric Belin <cedric.belin@mc2it.com>
2024-10-17 16:39:43 +0200

    Code optimization [skip ci]

--- castor.php
+++ castor.php
@@ -12,7 +12,6 @@ function context(): Context {
 #[AsTask(description: "Deletes all generated files")]
 function clean(): void {
 	fs()->remove(finder()->in("var"));
-	fs()->remove("www");
 }
 
 #[AsTask(description: "Performs the static analysis of source code")]
