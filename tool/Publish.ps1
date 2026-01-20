2983790d201202d5ab25d6c7227f892b04ab3a1e
Cédric Belin <cedx@outlook.com>
2026-01-20 19:15:06 +0100

    Code formatting [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,4 +1,4 @@
 "Publishing the package..."
-$version = (Get-Content "composer.json" | ConvertFrom-Json).version
+$version = Get-Content "composer.json" | ConvertFrom-Json | Select-Object -ExpandProperty version
 git tag "v$version"
 git push origin "v$version"
