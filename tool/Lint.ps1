f2620ce9cd6f02b85fc813088df5a7c5ed61f3ec
Cédric Belin <cedx@outlook.com>
2026-04-13 22:39:22 +0200

    Code formatting [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,5 @@
-"Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
+
+"Performing the static analysis of source code..."
 Invoke-ScriptAnalyzer $PSScriptRoot -ExcludeRule PSAvoidUsingPositionalParameters -Recurse
 composer exec "--" phpstan analyse --configuration=etc/PHPStan.php --memory-limit=256M --verbose
